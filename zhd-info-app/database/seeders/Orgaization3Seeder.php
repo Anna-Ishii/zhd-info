1ab316b153cdb4cb7439189d0198fa4e2a8689ab
mshodai <mshodai@nssx.co.jp>
2023-06-13 14:36:42 +0900

    organization3テーブルのマイグレーションファイルとシーダー作成

---
+++ zhd-info-app/database/seeders/Orgaization3Seeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class Orgaization3Seeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //　部署
+        DB::table('organization3')->insert(
+            [
+                [
+                    'name' => '東日本営業部',
+                    'organization2_id' => '0'
+                ],
+                [
+                    'name' => '中日本・関西営業部',
+                    'organization2_id' => '0'
+                ],
+                [
+                    'name' => '営業部',
+                    'organization2_id' => '3'
+                ]
+            ]
+        );
+    }
+}
