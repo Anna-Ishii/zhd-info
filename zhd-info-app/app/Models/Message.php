97c54a529c6021570f1002cd1c08f05dbe52c69a
mshodai <mshodai@nssx.co.jp>
2023-06-14 17:52:09 +0900

    モデルの作成とリレーション追加

--- zhd-info-app/app/Models/Message.php
+++ zhd-info-app/app/Models/Message.php
@@ -3,23 +3,27 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Message extends Model
 {
     protected $table = 'messages';
 
-    protected $fillable = 
-        ['title',
-         'content_url', 
-         'create_user',
-         'status',
-         'is_emergency',
-         'start_datatime',
-         'end_datatime',
-         'target_roll',
-         'target_block',
-        ];
+    protected $fillable =
+    [
+        'title',
+        'content_url',
+        'category_id',
+        'create_user',
+        'status',
+        'emergency_flg',
+        'start_datatime',
+        'end_datatime',
+        'target_roll',
+        'target_block',
+    ];
 
     // 多対多のリレーションを定義
     public function roll(): BelongsToMany
@@ -27,4 +31,19 @@ class Message extends Model
         return $this->belongsToMany(Roll::class, 'message_roll');
     }
 
+    public function user(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'message_user', 'employee_code', 'message_id')
+            ->withPivot('read_flg', 'shop_id');
+    }
+
+    public function create_user_detail(): HasOne
+    {
+        return $this->hasOne(User::class, 'create_user', 'employee_code');
+    }
+
+    public function category(): HasOne
+    {
+        return $this->hasOne(Category::class, foreignKey: 'id', localKey: 'category_id');
+    }
 }
--- zhd-info-app/app/Models/User.php
+++ zhd-info-app/app/Models/User.php
@@ -3,8 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class User extends Model
 {
-    protected $fillable = ['name', 'email', 'password'];
+    protected $primaryKey = 'employee_code';
+
+    protected $fillable = 
+        ['name',
+         'email',
+         'password',
+         'employee_code',
+         'shop_id',
+         'roll_id'
+        ];
+
+    public function message(): BelongsToMany
+    {
+        return $this->belongsToMany(Message::class, 'message_user', 'employee_code', 'message_id')
+                    ->withPivot('read_flg', 'shop_id');
+    }
 }
