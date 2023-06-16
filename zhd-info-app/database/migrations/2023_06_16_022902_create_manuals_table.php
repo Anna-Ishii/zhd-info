b052b8da5a1f9ab0dacfbaf706e9e564ea62c955
mshodai <mshodai@nssx.co.jp>
2023-06-16 15:12:10 +0900

    動画マニュアル関連のマイグレーションファイルとシーダーとモデルを作成

---
+++ zhd-info-app/database/migrations/2023_06_16_022902_create_manuals_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('manuals', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('content_url');
+            $table->integer('create_user_id');
+            $table->integer('category_id');
+            $table->integer('status')->defalt(0);
+            $table->dateTime('start_datetime')->nullable();
+            $table->dateTime('end_datetime')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('manuals');
+    }
+};
