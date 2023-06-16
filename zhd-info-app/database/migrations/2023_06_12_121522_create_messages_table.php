e9d6753b11b875a032dddbf2b1b95de69d84946f
mshodai <mshodai@nssx.co.jp>
2023-06-16 19:40:28 +0900

    マイグレーションファイル修正

--- zhd-info-app/database/migrations/2023_06_12_121522_create_messages_table.php
+++ zhd-info-app/database/migrations/2023_06_12_121522_create_messages_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('content_url');
             $table->integer('category_id');
             $table->integer('create_user_id');
-            $table->integer('status')->defalt(0);
+            $table->integer('status')->default(0);
             $table->boolean('emergency_flg');
             $table->dateTime('start_datetime')->nullable();
             $table->dateTime('end_datetime')->nullable();
