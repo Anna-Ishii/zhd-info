7253ffb2e660e01ab460b43567579eec7f090527
mshodai <mshodai@nssx.co.jp>
2023-06-19 15:40:17 +0900

    業連管理画面作成

--- zhd-info-app/app/Http/Controllers/Admin/Message/MessageManageController.php
+++ zhd-info-app/app/Http/Controllers/Admin/Message/MessageManageController.php
@@ -9,6 +9,7 @@ use App\Models\Category;
 use App\Models\Message;
 use App\Models\Organization4;
 use App\Models\Roll;
+use App\Models\Shop;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Queue\NullQueue;
@@ -18,11 +19,30 @@ class MessageManageController extends Controller
 {
     public function index()
     {
-        return view('admin.message.manage.index');
+        $category_list = Category::all();
+
+        // $message_list = $user->message;
+        $message_list = Message::all();
+
+        return view('admin.message.manage.index', [
+            'category_list' => $category_list,
+            'message_list' => $message_list
+
+        ]);
     }
 
     public function detail(Request $request, $message_id)
     {
-        return view('admin.message.manage.detail');
+        $message = Message::find($message_id);
+
+        // メッセージの該当ショップを取得
+        $target_org4 = $message->organization4()->select('id')->get()->makeHidden('pivot')->toArray();
+        $target_shop = Shop::whereIn('organization4_id', $target_org4)->get();
+
+
+        return view('admin.message.manage.detail', [
+            "message" => $message,
+            "target_shop" => $target_shop
+        ]);
     }
 }
