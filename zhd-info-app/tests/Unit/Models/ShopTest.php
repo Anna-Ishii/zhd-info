72f3294990fcf3a2a301b732a603a2f415e5f833
mshodai <mshodai@nssx.co.jp>
2023-06-21 16:08:50 +0900

    shopモデルのunitテスト追加

--- zhd-info-app/tests/Unit/Models/ShopTest.php
+++ zhd-info-app/tests/Unit/Models/ShopTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit\Models;
 
 use App\Models\Shop;
+use Symfony\Component\VarDumper\Caster\RdKafkaCaster;
 use Tests\TestCase;
 
 class ShopTest extends TestCase
@@ -13,5 +14,20 @@ class ShopTest extends TestCase
         $shop = Shop::find($shop_id);
 
         $this->assertEquals("北海道", $shop->organization4->name);
+    }
+
+    public function test_Shopモデルからユーザーの数を取得できるか()
+    {
+        $shop = Shop::withCount('user')->get();
+
+        $this->assertEquals(2, $shop[0]->user_count);
+    }
+
+    public function test_Shopモデルから条件指定でユーザーの数を取得できるか()
+    {
+        $shop_id = 1;
+        $shop = Shop::withCount('user')->whereIn('id', [1,2,3])->get();
+
+        $this->assertEquals(2, $shop[0]->user_count);
     }
 }
