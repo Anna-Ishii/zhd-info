37dd8d04e4278035f09e040686eb6b7319b75066
mshodai <mshodai@nssx.co.jp>
2023-06-23 15:05:31 +0900

    パスワード変更画面作成

--- zhd-info-app/resources/views/admin/setting/change_password.blade.php
+++ zhd-info-app/resources/views/admin/setting/change_password.blade.php
@@ -1,13 +1,29 @@
+@extends('layouts.admin.parent')
+
+@section('content')
 <div id="page-wrapper">
     <div class="row">
         <div class="col-lg-12">
             <h1 class="page-header">パスワード変更</h1>
         </div>
     </div>
-
+    @if (session('error'))
+    <div class="alert alert-danger">{{(session('error'))}}</div>
+    @endif
+    @if (session('message'))
+    <div class="alert alert-success">{{(session('message'))}}</div>
+    @endif
+    @if ($errors->any())
+    <div class="alert alert-danger">
+    <ul>
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+    </div>
+    @endif
     <form method="post" action="" class="form-horizontal">
-        <input type="hidden" name="mode" value="exec">
-
+        @csrf
         <div class="form-group">
             <label class="col-lg-2 control-label">現在のパスワード</label>
             <div class="col-lg-10">
@@ -22,9 +38,10 @@
         </div>
 
         <div class="text-center">
-            <input id="submitbutton" class="btn btn-danger" type="button" value="登　録" />
-            <a href="/message/publish/" class="btn btn-default">一覧に戻る</a>
+            <input id="submitbutton" class="btn btn-danger" type="submit" value="登　録" />
+            <a href="{{route('admin.account.index')}}" class="btn btn-default">一覧に戻る</a>
         </div>
 
     </form>
 </div>
+@endsection
