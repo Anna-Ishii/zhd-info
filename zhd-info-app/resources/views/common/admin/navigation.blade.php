128d18d55495b62f528997c87a16b0d8b0c91953
mshodai <mshodai@nssx.co.jp>
2023-06-23 15:52:35 +0900

    ログアウト機能

--- zhd-info-app/resources/views/common/admin/navigation.blade.php
+++ zhd-info-app/resources/views/common/admin/navigation.blade.php
@@ -19,7 +19,7 @@
             </a>
             <ul class="dropdown-menu dropdown-user">
                 <li><a href="{{ route('admin.setting.change_password.index') }}"><i class="fa fa-user"><span class="mr4">{{ $user->name }}</span></i> パスワード変更</a></li>
-                <li><a href="/auth/"><i class="fa fa-sign-out fa-fw"></i> ログアウト</a></li>
+                <li><a id="logout_btn"><i class="fa fa-sign-out fa-fw"></i> ログアウト</a></li>
             </ul>
             <!-- /.dropdown-user -->
         </li>
@@ -59,3 +59,4 @@
     </div>
     <!-- /.navbar-static-side -->
 </nav>
+<script src="{{asset('js/admin/navigation/index.js')}}"></script>
