071647e848b7f64d4c3097e5b903ba117853f82e
mshodai <mshodai@nssx.co.jp>
2023-06-24 21:20:20 +0900

    adminのmessageコントローラーのgetとpostの関数を分けた

--- zhd-info-app/app/Http/Controllers/Admin/Message/MessagePublishController.php
+++ zhd-info-app/app/Http/Controllers/Admin/Message/MessagePublishController.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\DB;
 
 class MessagePublishController extends Controller
 {
-    public function index(Request $request)
+    public function index()
     {
         
         $category_list = Category::all();
@@ -33,63 +33,8 @@ class MessagePublishController extends Controller
         ]);
     }
 
-    public function new(Request $request)
+    public function new()
     {
-        if ($request->isMethod('post')) {
-            $msg_params = $request
-                ->only([
-                    'title',
-                    'category_id',
-                ]);
-            $msg_params['emergency_flg'] = 
-                ($request->emergency_flg == 'on' ? true : false);
-            
-            if ($request->start_datetime == 'on') $request->start_datetime = null;
-            $msg_params['start_datetime'] =
-                !empty($request->start_datetime) ? Carbon::parse($request->start_datetime, 'Asia/Tokyo') : null;
-            
-            if ($request->end_datetime == 'on') $request->end_datetime = null;
-            $msg_params['end_datetime'] =
-                !empty($request->end_datetime) ? Carbon::parse($request->end_datetime, 'Asia/Tokyo') : null;
-
-
-            $file = $request->file('file');
-            $directory = 'uploads';
-            // ファイル名を生成します（一意の名前を使用する場合は、例えばユーザーIDやタイムスタンプを組み合わせることもできます）
-            $filename = uniqid() . '.' . $file->getClientOriginalExtension();
-
-            $path = public_path('uploads');
-            $file->move($path, $filename);
-            $content_url = 'uploads/' . $filename;
-            // ファイルを指定したディレクトリに保存します
-            // $path = $file->storeAs($directory, $filename, 'public');
-            $msg_params['content_url'] = $content_url;
-            $msg_params['create_user_id'] = session('user')->id;
-
-            $shops_id = Shop::select('id')->whereIn('organization4_id', $request->organization4)->get()->toArray();
-            $target_users = User::select('id', 'shop_id')->whereIn('shop_id', $shops_id)->whereIn('roll_id', $request->target_roll)->get()->toArray();
-
-            $data = [];
-            foreach ($target_users as $target_user) {
-                $data[$target_user['id']] = ['shop_id' => $target_user['shop_id']];
-            }
-
-            try {
-                $message = Message::create($msg_params);
-                $message->roll()->attach($request->target_roll);
-                $message->organization4()->attach($request->organization4);
-                $message->user()->attach($data);
-
-            } catch (\Throwable $th) {
-                return redirect()
-                    ->route('admin.message.publish.new')
-                    ->withInput()
-                    ->with('error', '入力エラーがあります');
-            }
-
-            return redirect()->route('admin.message.publish.index');
-        }
-
         $category_list = Category::all();
         // 「一般」は使わない
         $target_roll_list = Roll::where('id','!=','1')->get();
@@ -103,66 +48,63 @@ class MessagePublishController extends Controller
         ]);
     }
 
-    public function edit(Request $request, $message_id)
+    public function store(Request $request)
     {
-        if ($request->isMethod('post')) {
-            $msg_params = $request
-                ->only([
-                    'title',
-                    'category_id',
-                ]);
-            $msg_params['emergency_flg'] =
-                ($request->emergency_flg == 'on' ? true : false);
+        $msg_params = $request
+            ->only([
+                'title',
+                'category_id',
+            ]);
+        $msg_params['emergency_flg'] =
+        ($request->emergency_flg == 'on' ? true : false);
 
-            if ($request->start_datetime == 'on') $request->start_datetime = null;
-            $msg_params['start_datetime'] =
-                !empty($request->start_datetime) ? Carbon::parse($request->start_datetime, 'Asia/Tokyo') : null;
+        if ($request->start_datetime == 'on') $request->start_datetime = null;
+        $msg_params['start_datetime'] =
+        !empty($request->start_datetime) ? Carbon::parse($request->start_datetime, 'Asia/Tokyo') : null;
 
-            if ($request->end_datetime == 'on') $request->end_datetime = null;
-            $msg_params['end_datetime'] =
-                !empty($request->end_datetime) ? Carbon::parse($request->end_datetime, 'Asia/Tokyo') : null;
+        if ($request->end_datetime == 'on') $request->end_datetime = null;
+        $msg_params['end_datetime'] =
+        !empty($request->end_datetime) ? Carbon::parse($request->end_datetime, 'Asia/Tokyo') : null;
 
-            if($request->file('file')){
-                $file = $request->file('file');
-                $directory = 'uploads';
-                // ファイル名を生成します（一意の名前を使用する場合は、例えばユーザーIDやタイムスタンプを組み合わせることもできます）
-                $filename = uniqid() . '.' . $file->getClientOriginalExtension();
 
-                $path = public_path('uploads');
-                $file->move($path, $filename);
-                $content_url = 'uploads/' . $filename;
-                // ファイルを指定したディレクトリに保存します
-                // $path = $file->storeAs($directory, $filename, 'public');
-                $msg_params['content_url'] = $content_url;
-            }
-            $msg_params['create_user_id'] = session('user')->id;
+        $file = $request->file('file');
+        $directory = 'uploads';
+        // ファイル名を生成します（一意の名前を使用する場合は、例えばユーザーIDやタイムスタンプを組み合わせることもできます）
+        $filename = uniqid() . '.' . $file->getClientOriginalExtension();
 
-            $shops_id = Shop::select('id')->whereIn('organization4_id', $request->organization4)->get()->toArray();
-            $target_users = User::select('id', 'shop_id')->whereIn('shop_id', $shops_id)->whereIn('roll_id', $request->target_roll)->get()->toArray();
-            $data = [];
-            foreach ($target_users as $target_user) {
-                $data[$target_user['id']] = ['shop_id' => $target_user['shop_id']];
-            }
-            try {
-                DB::beginTransaction();
-                $message = Message::find($message_id);
-                $message->update($msg_params);
-                $message->roll()->sync($request->target_roll);
-                $message->organization4()->sync($request->organization4);
-                $message->user()->sync($data);
-                DB::commit();
-            } catch (\Throwable $th) {
-                DB::rollBack();
-                return redirect()
-                    ->route('admin.message.publish.edit', ['message_id' => $message_id])
-                    ->withInput()
-                    ->with('error', '入力エラーがあります');
-            }
+        $path = public_path('uploads');
+        $file->move($path, $filename);
+        $content_url = 'uploads/' . $filename;
+        // ファイルを指定したディレクトリに保存します
+        // $path = $file->storeAs($directory, $filename, 'public');
+        $msg_params['content_url'] = $content_url;
+        $msg_params['create_user_id'] = session('user')->id;
 
-            return redirect()->route('admin.message.publish.index');
+        $shops_id = Shop::select('id')->whereIn('organization4_id', $request->organization4)->get()->toArray();
+        $target_users = User::select('id', 'shop_id')->whereIn('shop_id', $shops_id)->whereIn('roll_id', $request->target_roll)->get()->toArray();
 
+        $data = [];
+        foreach ($target_users as $target_user) {
+            $data[$target_user['id']] = ['shop_id' => $target_user['shop_id']];
         }
 
+        try {
+            $message = Message::create($msg_params);
+            $message->roll()->attach($request->target_roll);
+            $message->organization4()->attach($request->organization4);
+            $message->user()->attach($data);
+        } catch (\Throwable $th) {
+            return redirect()
+                ->route('admin.message.publish.new')
+                ->withInput()
+                ->with('error', '入力エラーがあります');
+        }
+
+        return redirect()->route('admin.message.publish.index');
+    }
+
+    public function edit($message_id)
+    {
         $message = Message::find($message_id);
         if(empty($message)) return redirect()->route('admin.message.publish.index');
 
@@ -185,6 +127,64 @@ class MessagePublishController extends Controller
             'message_target_org4' => $target_orgs4
 
         ]);
+    }
+
+    public function update(Request $request, $message_id)
+    {
+        $msg_params = $request
+            ->only([
+                'title',
+                'category_id',
+            ]);
+        $msg_params['emergency_flg'] =
+            ($request->emergency_flg == 'on' ? true : false);
+
+        if ($request->start_datetime == 'on') $request->start_datetime = null;
+        $msg_params['start_datetime'] =
+            !empty($request->start_datetime) ? Carbon::parse($request->start_datetime, 'Asia/Tokyo') : null;
+
+        if ($request->end_datetime == 'on') $request->end_datetime = null;
+        $msg_params['end_datetime'] =
+            !empty($request->end_datetime) ? Carbon::parse($request->end_datetime, 'Asia/Tokyo') : null;
+
+        if ($request->file('file')) {
+            $file = $request->file('file');
+            $directory = 'uploads';
+            // ファイル名を生成します（一意の名前を使用する場合は、例えばユーザーIDやタイムスタンプを組み合わせることもできます）
+            $filename = uniqid() . '.' . $file->getClientOriginalExtension();
+
+            $path = public_path('uploads');
+            $file->move($path, $filename);
+            $content_url = 'uploads/' . $filename;
+            // ファイルを指定したディレクトリに保存します
+            // $path = $file->storeAs($directory, $filename, 'public');
+            $msg_params['content_url'] = $content_url;
+        }
+        $msg_params['create_user_id'] = session('user')->id;
+
+        $shops_id = Shop::select('id')->whereIn('organization4_id', $request->organization4)->get()->toArray();
+        $target_users = User::select('id', 'shop_id')->whereIn('shop_id', $shops_id)->whereIn('roll_id', $request->target_roll)->get()->toArray();
+        $data = [];
+        foreach ($target_users as $target_user) {
+            $data[$target_user['id']] = ['shop_id' => $target_user['shop_id']];
+        }
+        try {
+            DB::beginTransaction();
+            $message = Message::find($message_id);
+            $message->update($msg_params);
+            $message->roll()->sync($request->target_roll);
+            $message->organization4()->sync($request->organization4);
+            $message->user()->sync($data);
+            DB::commit();
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            return redirect()
+                ->route('admin.message.publish.edit', ['message_id' => $message_id])
+                ->withInput()
+                ->with('error', '入力エラーがあります');
+        }
+
+        return redirect()->route('admin.message.publish.index');
     }
 
     public function stop(Request $request)
