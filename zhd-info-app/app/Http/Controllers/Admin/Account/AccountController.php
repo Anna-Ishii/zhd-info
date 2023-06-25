3a8201900b0fc51cd909370f09e7b7057590f8e3
mshodai <mshodai@nssx.co.jp>
2023-06-26 08:03:12 +0900

    ユーザー削除機能追加

--- zhd-info-app/app/Http/Controllers/Admin/Account/AccountController.php
+++ zhd-info-app/app/Http/Controllers/Admin/Account/AccountController.php
@@ -84,4 +84,14 @@ class AccountController extends Controller
         return redirect()->route('admin.account.index');
     }
 
+    public function delete(Request $request)
+    {
+        $data = $request->json()->all();
+        $user = session('member');
+        if(in_array((string)$user->id, $data['user_id'], true)){
+            return response()->json(['message' => 'ログイン中のユーザーは削除できません'], status:500);
+        }
+        User::whereIn('id', $data['user_id'])->delete();
+        return response()->json(['message' => '削除しました'], status: 200);
+    }
 }
--- zhd-info-app/public/js/admin/account/index.js
+++ zhd-info-app/public/js/admin/account/index.js
@@ -1,4 +1,47 @@
 $("#deleteBtn").on('click', function (e) {
     e.preventDefault();
-    
-});
+    console.log("delete");
+
+    var csrfToken = $('meta[name="csrf-token"]').attr('content');
+    var checkedCheckboxes = $(".form-check-input:checked");
+
+    if(checkedCheckboxes.length < 1) {
+        alert("ユーザーを選択してください") ;
+        return;
+    }
+
+    let checkedValues = checkedCheckboxes.map(function() {
+        var row = $(this).closest('tr');
+        return row.find('.user_id').text();
+    }).get();
+
+    fetch("/admin/account/delete", {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "X-CSRF-TOKEN": csrfToken
+        },
+        body: JSON.stringify({
+            'user_id': checkedValues
+        })
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            return response.json().then(data => {
+                throw new Error(data.message); // エラーメッセージをスロー
+            });
+        }})
+        .then(data => {
+            const message = data.message;
+            // メッセージの表示や処理を行う
+            alert(message);
+            window.location.reload();
+        })
+        .catch(error => {
+            const message = error.message;
+            alert(message);
+        })    
+    })
+
