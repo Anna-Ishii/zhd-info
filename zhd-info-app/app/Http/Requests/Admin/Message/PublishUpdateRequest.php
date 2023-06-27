6e9ad71c86f3d023a05a7e6e95d60a8dd95ee00c
mshodai <mshodai@nssx.co.jp>
2023-06-27 19:13:27 +0900

    ファイル名を一部大文字に変更

---
+++ zhd-info-app/app/Http/Requests/Admin/Message/PublishUpdateRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Admin\Message;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PublishUpdateRequest extends FormRequest
+{
+    public function rules()
+    {
+        return [
+            'title' => 'required',
+            'file'  => 'mimes:pdf|max:150000',
+            'category_id' => 'required',
+            'emergency_flg' => 'nullable',
+            'start_datetime' => 'nullable',
+            'end_datetime' => 'nullable',
+            'organization4' => 'nullable',
+            'target_roll' => 'nullable',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'title.required' => 'タイトルは必須項目です',
+            'file.mimes' => 'PDF形式のファイルを添付してください',
+            'file.max' => 'ファイルの容量が大きすぎます。150MB以下にしてください',
+            'category_id.required' => 'カテゴリを選択してください',
+        ];
+    }
+}
