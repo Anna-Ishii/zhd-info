d6ba959b249877d6476563c1be3068a9a14b4e39
mshodai <mshodai@nssx.co.jp>
2023-06-30 11:31:09 +0900

    検索フォーム全体をcomponentにした

--- zhd-info-app/resources/views/livewire/admin/account-search-form.blade.php
+++ zhd-info-app/resources/views/livewire/admin/account-search-form.blade.php
@@ -1,20 +1,43 @@
-<span>
-    <div class="input-group col-lg-2 spMb16">
-        <label class="input-group-addon">ブランド</label>
-        <select name="organization2" class="form-control"  wire:model="brand">
-            <option value=""> -- 指定なし -- </option>
-            @foreach ($organization2_list as $organization2)
-                <option value="{{$organization2->id}}" {{ $organization2->id == $brand ? 'selected' : ''}}>{{$organization2->name}}</option>
-            @endforeach
-        </select>
+<form method="get" class="form-horizontal mb24">
+    @csrf
+    <div class="form-group form-inline mb16">
+        <div class="input-group col-lg-2 spMb16">
+            <input name="q" value="{{$current_q}}" class="form-control" />
+            <span class="input-group-btn"><button class="btn btn-default" type="button" ><i class="fa fa-search"></i></button></span>
+        </div>
+
+        <div class="input-group col-lg-2 spMb16">
+            <label class="input-group-addon">ブランド</label>
+            <select name="organization2" class="form-control"  wire:model="current_organization2">
+                <option value=""> -- 指定なし -- </option>
+                @foreach ($organization2_list as $organization2)
+                    <option value="{{$organization2->id}}" {{ $organization2->id == $current_organization2 ? 'selected' : ''}}>{{$organization2->name}}</option>
+                @endforeach
+            </select>
+        </div>
+        <div class="input-group col-lg-2">
+            <label class="input-group-addon">店舗</label>
+            <select name="shop" class="form-control" wire:model="current_shop">
+                <option value=""> -- 指定なし -- </option>
+                @foreach ($shop_list as $shop)
+                    <option value="{{$shop->id}}" {{ $shop->id == $current_shop? "selected" : ""}}>{{$shop->name}}</option>
+                @endforeach
+            </select>
+        </div>
+        <div class="input-group col-lg-2">
+            <label class="input-group-addon">権限</label>
+            <select name="roll" class="form-control">
+                <option value=""> -- 指定なし -- </option>
+                @foreach ($roll_list as $roll)
+                    <option value="{{$roll->id}}" {{ $roll->id == $current_roll ? "selected" : "" }}>{{$roll->name}}</option>
+                @endforeach
+            </select>
+        </div>
+
     </div>
-    <div class="input-group col-lg-2">
-        <label class="input-group-addon">店舗</label>
-        <select name="shop" class="form-control" wire:model="shop_id">
-            <option value=""> -- 指定なし -- </option>
-            @foreach ($shop_list as $shop)
-                <option value="{{$shop->id}}" {{ $shop_id == $shop->id ? "selected" : ""}}>{{$shop->name}}</option>
-            @endforeach
-        </select>
+
+    <div class="text-center">
+        <button class="btn btn-info">検索</button>
     </div>
-</span>
+
+</form>
