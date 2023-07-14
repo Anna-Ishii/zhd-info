a37077ab228fee5d2c43733e9113373061831d35
mshodai <mshodai@nssx.co.jp>
2023-07-14 21:55:06 +0900

    top画面で週ごとの表示と、未読のものの表示

--- zhd-info-app/resources/views/top.blade.php
+++ zhd-info-app/resources/views/top.blade.php
@@ -19,17 +19,17 @@
 <main class="main">
     <div class="main__inner">
         <aricle class="indexList mb32">
-            <h2 class="mb10"><span class="txtBlue">本日到着した</span>お知らせ</h2>
+            <h2 class="mb10"><span class="txtBlue">今週の</span>お知らせ</h2>
             <div class="indexList__inner">
                 <div class="flex">
-                    @foreach($message_now as $ms_now)
-                    <a href="{{ asset($ms_now->content_url) }}">
+                    @foreach($message_now as $ms)
+                    <a href="{{ asset($ms->content_url) }}">
                         <div class="indexList__box">
-                            <p class="indexList__box__title txtBold">{{ $ms_now->title }}</p>
+                            <p class="indexList__box__title txtBold">{{ $ms->title }}</p>
                             <picture class="indexList__box__img">
-                                <img src=" {{ ($ms_now->thumbnails_url) ? asset($ms_now->thumbnails_url) : asset('/img/pdf_thumb_example.jpg') }}" alt="" class="mb14">
+                                <img src=" {{ ($ms->thumbnails_url) ? asset($ms->thumbnails_url) : asset('/img/pdf_thumb_example.jpg') }}" alt="" class="mb14">
                             </picture>
-                            <p class="indexList__box__title txtBold">{{ $ms_now->start_datetime }}</p>
+                            <p class="indexList__box__title txtBold">{{ $ms->start_datetime }}</p>
                         </div>
                     </a>
                     @endforeach
@@ -37,25 +37,65 @@
             </div>
         </aricle>
         <aricle class="indexList mb32">
-            <h2 class="mb10"><span class="txtBlue">本日到着した</span>動画マニュアル</h2>
+            <h2 class="mb10"><span class="txtBlue">今週の</span>動画マニュアル</h2>
             <div class="indexList__inner">
                 <div class="flex">
-                    @foreach($manual_now as $ml_now)
-                    <a href="{{ route('manual.detail', ['manual_id' => $ml_now->id]) }}">
+                    @foreach($manual_now as $ml)
+                    <a href="{{ route('manual.detail', ['manual_id' => $ml->id]) }}">
                         <div class="indexList__box">
+                            <p class="indexList__box__title txtBold">{{ $ml->title }}</p>
                             <picture class="indexList__box__img">
-                                <img src=" {{ ($ml_now->thumbnails_url) ? asset($ml_now->thumbnails_url) : asset('img/img_manual_dummy.jpg') }}" alt="" class="mb14">
+                                <img src=" {{ ($ml->thumbnails_url) ? asset($ml->thumbnails_url) : asset('img/img_manual_dummy.jpg') }}" alt="" class="mb14">
                             </picture>
-                            <p class="indexList__box__title txtBold">{{ $ml_now->title }}</p>
+                            <p class="indexList__box__title txtBold">{{ $ml->start_datetime }}</p>
                         </div>
-                        <button class="indexList_box_button list__box__tag">詳細を確認する</button>
+                        
                     </a>
 
                     @endforeach
                 </div>
             </div>
         </aricle>
+        <aricle class="indexList mb32">
+            <h2 class="mb10"><span class="txtBlue">未読の</span>業務連絡</h2>
+            <div class="indexList__inner">
+                <div class="flex">
+                    @foreach($message_unread as $ms)
+                    <a href="{{ asset($ms->content_url) }}">
+                        <div class="indexList__box">
+                            <p class="indexList__box__title txtBold">{{ $ms->title }}</p>
+                            <picture class="indexList__box__img">
+                                <img src=" {{ ($ms->thumbnails_url) ? asset($ms->thumbnails_url) : asset('img/img_manual_dummy.jpg') }}" alt="" class="mb14">
+                            </picture>
+                            <p class="indexList__box__title txtBold">{{ $ms->start_datetime }}</p>
+                        </div>
+                        
+                    </a>
 
+                    @endforeach
+                </div>
+            </div>
+        </aricle>
+        <aricle class="indexList mb32">
+            <h2 class="mb10"><span class="txtBlue">未読の</span>動画マニュアル</h2>
+            <div class="indexList__inner">
+                <div class="flex">
+                    @foreach($manual_unread as $ml)
+                    <a href="{{ route('manual.detail', ['manual_id' => $ml->id]) }}">
+                        <div class="indexList__box">
+                            <p class="indexList__box__title txtBold">{{ $ml->title }}</p>
+                            <picture class="indexList__box__img">
+                                <img src=" {{ ($ml->thumbnails_url) ? asset($ml->thumbnails_url) : asset('img/img_manual_dummy.jpg') }}" alt="" class="mb14">
+                            </picture>
+                            <p class="indexList__box__title txtBold">{{ $ml->start_datetime }}</p>
+                        </div>
+                        
+                    </a>
+
+                    @endforeach
+                </div>
+            </div>
+        </aricle>
     </div>
 </main>
 
