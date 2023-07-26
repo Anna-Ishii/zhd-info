b7bc2b3341b5999acb49db643fcf9a9affa7b5e2
mshodai <mshodai@nssx.co.jp>
2023-07-26 12:51:43 +0900

    添付ファイルのルールの記述の文言修正

--- zhd-info-app/app/Http/Requests/Admin/Manual/PublishUpdateRequest.php
+++ zhd-info-app/app/Http/Requests/Admin/Manual/PublishUpdateRequest.php
@@ -7,7 +7,6 @@ use Illuminate\Foundation\Http\FormRequest;
 class PublishUpdateRequest extends FormRequest
 {
     private $uploadableFileTypes = [
-        'avi' => 'video/x-msvideo',
         'mov' => 'video/quicktime',
         'mp4' => 'video/mp4',
         'm4v' => 'video/x-m4v',
@@ -38,13 +37,13 @@ class PublishUpdateRequest extends FormRequest
     {
         return [
             'title.required' => 'タイトルは必須項目です',
-            'file.mimetypes' => 'mp4,mov,jpeg,png,jpg,pdf形式のファイルを添付してください',
+            'file.mimetypes' => 'mp4,mov,m4v,jpeg,jpg,png,pdf形式のファイルを添付してください',
             'file' => 'ファイルのアップデートに失敗しました',
             'file.max' => 'ファイルの容量が大きすぎます。150MB以下にしてください',
             'category_id.required' => 'カテゴリを選択してください',
             'brand.required' => '対象業態を選択してください',
             'manual_flow_title.*.required_with' => '手順名は必須項目です',
-            'manual_file.*.mimetypes' => '手順ファイルはmp4,mov,png,jpeg,jpg,pdf形式のファイルを添付してください',
+            'manual_file.*.mimetypes' => '手順ファイルはmp4,mov,m4v,jpeg,jpg,png,pdf形式のファイルを添付してください',
             'manual_file.*' => '手順ファイルのアップロードに失敗しました'
         ];
     }
