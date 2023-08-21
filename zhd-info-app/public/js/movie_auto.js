960ff2b0f887b4d6b1a03e0466de31e68ab21da5
mshodai <mshodai@nssx.co.jp>
2023-08-21 10:59:12 +0900

    自動再生しない対応

--- zhd-info-app/public/js/movie_auto.js
+++ zhd-info-app/public/js/movie_auto.js
@@ -1,10 +1,8 @@
 "use strict";
 
-/* ページ読み込み時に自動再生 */
-let targetAutoPlay = $('.manualAttachment:visible video');
-$(window).on('DOMContentLoaded' , function(){
-	targetAutoPlay.removeClass('isPaused');
-	$('.manualAttachment:visible').find('.txtPlay').hide();
-	$('.manualAttachment:visible').find('.txtPause').show();
-	targetAutoPlay.get(0).play();
+/* ページ読み込み時にリサイズ */
+$(window).on('load' , function(){
+	if($('.manualAttachment.isActive').find('video').length){
+		changeMovieUIsize();
+	}
 });
