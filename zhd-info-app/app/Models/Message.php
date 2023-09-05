960b05da534cd4a2fad6e93893684fc91861f996
mshodai <mshodai@nssx.co.jp>
2023-09-05 16:37:02 +0900

    ページネーション作成

--- zhd-info-app/app/Models/Message.php
+++ zhd-info-app/app/Models/Message.php
@@ -49,13 +49,13 @@ class Message extends Model
     public function user(): BelongsToMany
     {
         return $this->belongsToMany(User::class, 'message_user','message_id', 'user_id')
-            ->withPivot('read_flg', 'shop_id');
+            ->withPivot('read_flg', 'shop_id', 'readed_datetime');
     }
     public function readed_user(): BelongsToMany
     {
         return $this->belongsToMany(User::class, 'message_user', 'message_id', 'user_id')
             ->wherePivot('read_flg', true)
-            ->withPivot('read_flg', 'shop_id');
+            ->withPivot('read_flg', 'shop_id', 'readed_datetime');
     }
 
     public function create_user(): HasOne
