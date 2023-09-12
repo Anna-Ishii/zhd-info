0a69857e72f89bab4f13d16a34bf15ddffd16b67
mshodai <mshodai@nssx.co.jp>
2023-09-12 18:09:25 +0900

    業務連絡の詳細画面のn+1を修正

--- zhd-info-app/app/Http/Controllers/Admin/Message/MessagePublishController.php
+++ zhd-info-app/app/Http/Controllers/Admin/Message/MessagePublishController.php
@@ -152,6 +152,7 @@ class MessagePublishController extends Controller
 
         $user_list = $message
                         ->user()
+                        ->with(['shop', 'shop.organization3','shop.organization4', 'shop.organization5'])
                         ->when(isset($read_flg), function ($query) use ($read_flg) {
                             if($read_flg == 'true') $query->where('read_flg', true);
                             if($read_flg == 'false') $query->where('read_flg', false);
