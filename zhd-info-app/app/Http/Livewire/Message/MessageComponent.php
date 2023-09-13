7a8a833a8bc627434066be3d4c249bf2d6090773
mshodai <mshodai@nssx.co.jp>
2023-09-13 13:27:25 +0900

    店舗側画面の既読をつける処理を修正

--- zhd-info-app/app/Http/Livewire/Message/MessageComponent.php
+++ zhd-info-app/app/Http/Livewire/Message/MessageComponent.php
@@ -2,22 +2,22 @@
 
 namespace App\Http\Livewire\Message;
 
+use App\Models\Message;
 use Carbon\Carbon;
 use Livewire\Component;
-
 class MessageComponent extends Component
 {
-    public $message;
+    public Message $message;
 
     public function reading()
     {
         $member = session("member");
         // 既読をつける
-        $member->message()->updateExistingPivot($this->message->id, [
+        $member->message()->wherePivot('read_flg', false)->updateExistingPivot($this->message->id, [
             'read_flg' => true,
             'readed_datetime' => Carbon::now(),
         ]);
-        return redirect()->to(route('message.index'));
+        return redirect()->to($this->message->content_url);
     }
 
     public function render()
