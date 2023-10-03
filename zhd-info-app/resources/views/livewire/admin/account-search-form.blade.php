4c80e171269b36713c81185216d4d0c7e8dda726
mshodai <mshodai@nssx.co.jp>
2023-10-03 20:16:38 +0900

    レイアウト修正

--- zhd-info-app/resources/views/livewire/admin/account-search-form.blade.php
+++ zhd-info-app/resources/views/livewire/admin/account-search-form.blade.php
@@ -4,7 +4,7 @@
         <div class="input-group col-lg-2 spMb16">
             <label class="input-group-addon">ブランド</label>
             <select name="organization2" class="form-control"  wire:model="current_organization2">
-                <option value=""> -- 指定なし -- </option>
+                <option value="">指定なし</option>
                 @foreach ($organization2_list as $organization2)
                     <option value="{{$organization2->id}}" {{ $organization2->id == $current_organization2 ? 'selected' : ''}}>{{$organization2->name}}</option>
                 @endforeach
@@ -13,7 +13,7 @@
         <div class="input-group col-lg-2 spMb16">
             <label class="input-group-addon">店舗</label>
             <select name="shop" class="form-control" wire:model="current_shop">
-                <option value=""> -- 指定なし -- </option>
+                <option value="">指定なし</option>
                 @foreach ($shop_list as $shop)
                     <option value="{{$shop->id}}" {{ $shop->id == $current_shop? "selected" : ""}}>{{$shop->name}}</option>
                 @endforeach
@@ -22,7 +22,7 @@
         <div class="input-group col-lg-2 spMb16">
             <label class="input-group-addon">権限</label>
             <select name="roll" class="form-control">
-                <option value=""> -- 指定なし -- </option>
+                <option value="">指定なし</option>
                 @foreach ($roll_list as $roll)
                     <option value="{{$roll->id}}" {{ $roll->id == $current_roll ? "selected" : "" }}>{{$roll->name}}</option>
                 @endforeach
