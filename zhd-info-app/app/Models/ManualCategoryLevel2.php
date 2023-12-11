1db02e7f985a9b760dafc8a488bd7188edac593d
mshodai <mshodai@nssx.co.jp>
2023-12-11 16:24:51 +0900

    カテゴリのマイグレーションファイルとモデルを作成

---
+++ zhd-info-app/app/Models/ManualCategoryLevel2.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ManualCategoryLevel2 extends Model
+{
+    use HasFactory;
+}
