2853f93eee3cd6b9e45b66fe836f92c3e0de95ea
mshodai <mshodai@nssx.co.jp>
2023-12-13 21:03:49 +0900

    新カテゴリのマイグレーションファイル編集と、モデルファイル作成

--- zhd-info-app/app/Models/ManualCategoryLevel2.php
+++ zhd-info-app/app/Models/ManualCategoryLevel2.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class ManualCategoryLevel2 extends Model
 {
     use HasFactory;
+
+    public function level1():HasOne
+    {
+        return $this->hasOne(ManualCategoryLevel1::class, 'level1', 'id');
+    }
 }
