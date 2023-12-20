49178f85cff8ebae3abde2bf66e35764a551f1f2
mshodai <mshodai@nssx.co.jp>
2023-12-20 14:44:22 +0900

    assetsを配置

--- zhd-info-app/public/js/common.js
+++ zhd-info-app/public/js/common.js
@@ -26,3 +26,52 @@ $(document).on('click' , '.btnAddLabel' , function(){
 	}
 });
 
+"use strict";
+
+$(document).on('click' , '.btnSort', function(){
+	$('.sortMenu').addClass('isActive');
+});
+
+/* ソートカテゴリ選択 */
+$(document).on('change' , '.selectAll' , function(){
+	let chkStatus = $(this).prop('checked');
+	let target = $(this).parents('.sortMenu__box').find('input[type=checkbox]');
+	if(chkStatus){
+		target.each(function(){
+			if(!$(this).prop('checked')){
+				$(this).prop('checked' , true);
+			}
+		});
+	}else{
+		target.each(function(){
+			if($(this).prop('checked')){
+				$(this).prop('checked' , false);
+			}
+		});
+	}
+});
+
+$(document).on('change' , '.sortMenu__list__item input[type=checkbox]' , function(){
+	let chkTarget = $(this).parents('.sortMenu__list').find('input[type=checkbox]');
+	/* 一回チェックを外す */
+	$(this).parents('.sortMenu__box').find('.selectAll').prop('checked' , false);
+	chkTarget.each(function(){
+		if($(this).prop('checked')){
+			$(this).parents('.sortMenu__box').find('.selectAll').prop('checked' , true);
+			return false;
+		}
+	});
+});
+
+$(document).on('click' , '.btnSearchReset', function(){
+	let target = $('.sortMenu').find('input[type=checkbox]');
+	target.each(function(){
+		$(this).prop('checked' , false);
+	});
+});
+
+/* 検索処理（仮置き） */
+$(document).on('click' , '.btnSearch', function(){
+	$('.sortMenu').removeClass('isActive');
+});
+
