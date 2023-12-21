f4cca522007611b81b93c4583adc945459b9e0ae
mshodai <mshodai@nssx.co.jp>
2023-12-22 07:02:48 +0900

    新カテゴリのリレーション修正

--- zhd-info-app/app/Models/ManualCategoryLevel2.php
+++ zhd-info-app/app/Models/ManualCategoryLevel2.php
@@ -4,14 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class ManualCategoryLevel2 extends Model
 {
     use HasFactory;
 
-    public function level1():HasOne
+    protected $fillable =
+    [
+        'name',
+        'level1'
+    ];
+
+    public function level1():BelongsTo
     {
-        return $this->hasOne(ManualCategoryLevel1::class, 'level1', 'id');
+        return $this->belongsTo(ManualCategoryLevel1::class, 'level1', 'id');
     }
 }
