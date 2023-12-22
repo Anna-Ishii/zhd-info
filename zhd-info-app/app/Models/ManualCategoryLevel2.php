8b792189d68d110ae31b285395fd505a335c8bc4
mshodai <mshodai@nssx.co.jp>
2023-12-22 11:47:54 +0900

    リレーション修正

--- zhd-info-app/app/Models/ManualCategoryLevel2.php
+++ zhd-info-app/app/Models/ManualCategoryLevel2.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class ManualCategoryLevel2 extends Model
@@ -20,5 +21,10 @@ class ManualCategoryLevel2 extends Model
     public function level1():BelongsTo
     {
         return $this->belongsTo(ManualCategoryLevel1::class, 'level1', 'id');
+    }
+
+    public function manuals():HasMany
+    {
+        return $this->hasMany(Manual::class, 'category_level2_id', 'id');
     }
 }
