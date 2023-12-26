31fc006317eb7182a56ddc70dfae67198226461e
mshodai <mshodai@nssx.co.jp>
2023-12-26 16:00:47 +0900

    ページネーションの実装

--- zhd-info-app/resources/views/common/navigation.blade.php
+++ zhd-info-app/resources/views/common/navigation.blade.php
@@ -8,21 +8,37 @@
     <div class="result__flexBox">
         <p>全 {{$objects->total()}} 件中 {{$min}} 件 〜 {{$max}} 件を表示</p>
         <ul class="result__pager">
-    <li><a href="{{ $objects->previousPageUrl() }}" class="result__pager__prev">前へ</a></li>
-    @for ($i = 1; $i <= ceil($objects->total() / $objects->perPage()); $i++)
+            <li><a href="{{ $objects->previousPageUrl() }}" class="result__pager__prev">前へ</a></li>
 
-        <li><a href="{{$objects->url($i)}}" class="{{$objects->currentPage() == $i ? 'isCurrent' : ''}}">{{$i}}</a></li>
-        @if ($i > 3)
+    @if ($currentPage > 3)
             <li>...</li>
+    @endif
+    @for ($i = 1; $i <= $totalPage; $i++)
+        @if ($currentPage <= 3)
+            <li>
+                <a href="{{$objects->url($i)}}" class="{{$currentPage == $i ? 'isCurrent' : ''}}">{{$i}}</a>
+            </li>
+        @elseif($currentPage > $totalPage - 3)
+            <li>
+                <a href="{{$objects->url(($totalPage - 3) + $i)}}" class="{{$currentPage == ($totalPage - 3) + $i ? 'isCurrent' : ''}}">
+                    {{($totalPage - 3) + $i}}
+                </a>
+            </li>
+        @else
+            <li>
+                <a href="{{$objects->url($currentPage + $i - 3)}}" class="{{$currentPage == $currentPage + $i - 3  ? 'isCurrent' : ''}}">
+                    {{$currentPage + $i - 3}}
+                </a>
+            </li>
+        @endif
+        @if ($i > 3)
             @break
         @endif
     @endfor
-    
-            <li><a href=
-                {{-- @if()
-                @elseif()
-                @endif --}}
-                "{{ $objects->nextPageUrl() }}"
+    @if ($currentPage  <= $totalPage - 3)
+            <li>...</li>
+    @endif
+            <li><a href= "{{ $objects->nextPageUrl() }}"
                 class="result__pager__next">次へ</a>
             </li>        
         </ul>
