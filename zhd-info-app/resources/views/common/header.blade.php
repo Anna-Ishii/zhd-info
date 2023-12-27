44164cf899992b35db1c5ac934227a58216b92d8
mshodai <mshodai@nssx.co.jp>
2023-12-27 19:20:47 +0900

    ヘッダーの修正

--- zhd-info-app/resources/views/common/header.blade.php
+++ zhd-info-app/resources/views/common/header.blade.php
@@ -1,9 +1,9 @@
 <header class="header">
-  {{-- <div class="header__inner">
-        <select name="" class="header__name">
+  <div class="header__inner">
+        {{-- <select name="" class="header__name">
             <option selected>名前を選択してください</option>
             <option value="1">テキストテキストテキスト</option>
             <option value="2">テキストテキストテキスト</option>
-        </select>
-    </div> --}}
+        </select> --}}
+    </div>
 </header>
