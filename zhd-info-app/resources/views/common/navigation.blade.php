cd31834f389d33b5e23a3db78bddd4f66b9707b8
mshodai <mshodai@nssx.co.jp>
2024-01-10 11:29:15 +0900

    フォーマッターでインデント調整

--- zhd-info-app/resources/views/common/navigation.blade.php
+++ zhd-info-app/resources/views/common/navigation.blade.php
@@ -6,54 +6,57 @@
         $min = ($currentPage - 1) * 20 + 1;
     @endphp
     <div class="result__flexBox">
-    @if ($objects->total() == 0)
-        <p>全 0 件</p>
-    @else
-        <p>全 {{$objects->total()}} 件中 {{$min}} 件 〜 {{$max}} 件を表示</p>
-    @endif
+        @if ($objects->total() == 0)
+            <p>全 0 件</p>
+        @else
+            <p>全 {{ $objects->total() }} 件中 {{ $min }} 件 〜 {{ $max }} 件を表示</p>
+        @endif
         <ul class="result__pager">
-        @if ($totalPage > 1)
-            <li><a href="{{ $objects->url($objects->url(1)) }}" class="result__pager__first"></a></li>
-            <li><a href="{{ $objects->previousPageUrl() }}" class="result__pager__prev"></a></li>
-        @else
-            <li></li>
-            <li></li>
-        @endif
-        @if ( $totalPage <= 4)
-            @for ($i = 1; $i <= ceil($objects->total() / $objects->perPage()); $i++)
-                <li>
-                    <a href="{{ $objects->url($i) }}" class="{{$currentPage == $i ? 'isCurrent' : ''}}">{{$i}}</a>
-                </li>
-            @endfor
-        @else
-
-            @for ($i = 1; $i <= 4; $i++)
-                @if ($currentPage <= 3)
+            @if ($totalPage > 1)
+                <li><a href="{{ $objects->url($objects->url(1)) }}" class="result__pager__first"></a></li>
+                <li><a href="{{ $objects->previousPageUrl() }}" class="result__pager__prev"></a></li>
+            @else
+                <li></li>
+                <li></li>
+            @endif
+            @if ($totalPage <= 4)
+                @for ($i = 1; $i <= ceil($objects->total() / $objects->perPage()); $i++)
                     <li>
-                        <a href="{{$objects->url($i)}}" class="{{$currentPage == $i ? 'isCurrent' : ''}}">{{$i}}</a>
+                        <a href="{{ $objects->url($i) }}"
+                            class="{{ $currentPage == $i ? 'isCurrent' : '' }}">{{ $i }}</a>
                     </li>
-                @elseif($currentPage > $totalPage - 3)
-                    <li>
-                        <a href="{{$objects->url(($totalPage - 4) + $i)}}" class="{{$currentPage == ($totalPage - 4) + $i ? 'isCurrent' : ''}}">
-                            {{($totalPage - 4) + $i}}
-                        </a>
-                    </li>
-                @else
-                    <li>
-                        <a href="{{$objects->url($currentPage + $i - 3)}}" class="{{$currentPage == $currentPage + $i - 3  ? 'isCurrent' : ''}}">
-                            {{$currentPage + $i - 3}}
-                        </a>
-                    </li>
-                @endif
-            @endfor
-        @endif
-        @if ($totalPage > 1)
-            <li><a href= "{{ $objects->nextPageUrl() }}"class="result__pager__next"></a></li>
-            <li><a href="{{ $objects->url($objects->lastPage()) }}" class="result__pager__last"></a></li>
-        @else
-            <li></li>
-            <li></li>
-        @endif
+                @endfor
+            @else
+                @for ($i = 1; $i <= 4; $i++)
+                    @if ($currentPage <= 3)
+                        <li>
+                            <a href="{{ $objects->url($i) }}"
+                                class="{{ $currentPage == $i ? 'isCurrent' : '' }}">{{ $i }}</a>
+                        </li>
+                    @elseif($currentPage > $totalPage - 3)
+                        <li>
+                            <a href="{{ $objects->url($totalPage - 4 + $i) }}"
+                                class="{{ $currentPage == $totalPage - 4 + $i ? 'isCurrent' : '' }}">
+                                {{ $totalPage - 4 + $i }}
+                            </a>
+                        </li>
+                    @else
+                        <li>
+                            <a href="{{ $objects->url($currentPage + $i - 3) }}"
+                                class="{{ $currentPage == $currentPage + $i - 3 ? 'isCurrent' : '' }}">
+                                {{ $currentPage + $i - 3 }}
+                            </a>
+                        </li>
+                    @endif
+                @endfor
+            @endif
+            @if ($totalPage > 1)
+                <li><a href= "{{ $objects->nextPageUrl() }}"class="result__pager__next"></a></li>
+                <li><a href="{{ $objects->url($objects->lastPage()) }}" class="result__pager__last"></a></li>
+            @else
+                <li></li>
+                <li></li>
+            @endif
         </ul>
     </div>
 </div>
