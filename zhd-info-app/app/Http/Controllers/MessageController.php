504e3d1eb2750f9dcd5b2a0ba9b67b19aad7b453
mshodai <mshodai@nssx.co.jp>
2024-02-13 21:14:57 +0900

    [WIP]閲覧率を表示

--- zhd-info-app/app/Http/Controllers/MessageController.php
+++ zhd-info-app/app/Http/Controllers/MessageController.php
@@ -17,10 +17,33 @@ class MessageController extends Controller
         $search_period = SearchPeriod::tryFrom($request->input('search_period', SearchPeriod::All->value));
 
         $user = session("member");
+
+        $sub = DB::table('messages')
+                    ->select([
+                        DB::raw('messages.id as message_id'),
+                        DB::raw('count(c.id) as crew_count'),
+                        DB::raw('count(c_m_l.crew_id) as readed_crew_count'),
+                        DB::raw('round((count(c_m_l.crew_id) / count(c.id)) * 100, 0) as view_rate')
+                    ])
+                    ->leftjoin('message_user as m_u', 'messages.id', '=', 'm_u.message_id')
+                    ->leftjoin('crews as c', 'm_u.user_id', '=', 'c.user_id')
+                    ->leftjoin('crew_message_logs as c_m_l', function($join){
+                        $join->on('c_m_l.crew_id', '=', 'c.id')
+                            ->where('c_m_l.message_id', '=', DB::raw('messages.id'));
+                    })
+                    ->where('m_u.user_id', '=', $user->id)
+                    ->groupBy('messages.id');
+
         // 掲示中のデータをとってくる
         $messages = $user->message()
             ->with('category', 'tag')
+            ->select([
+                'sub.crew_count as crew_count',
+                'sub.readed_crew_count as readed_crew_count',
+                'sub.view_rate as view_rate'
+                ])
             ->publishingMessage()
+            ->JoinSub($sub, 'sub', 'messages.id', 'sub.message_id')
             ->when(isset($keyword), function ($query) use ($keyword) {
                 $query->where(function ($query) use ($keyword) {
                     $query->whereLike('title', $keyword)
