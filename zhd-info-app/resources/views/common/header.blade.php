fda156f4de3e83872c9cbac5e73506572a57d509
mshodai <mshodai@nssx.co.jp>
2024-02-20 11:33:01 +0900

    レイアウト調整

--- zhd-info-app/resources/views/common/header.blade.php
+++ zhd-info-app/resources/views/common/header.blade.php
@@ -1,11 +1,13 @@
 <header class="header">
   <div class="header__inner">
     @if (session('check_crew'))
-      <div>{{$check_crew[0]->part_code ?? ""}} {{$check_crew[0]->name ?? ""}}さんの未読/既読を表示中です。</div>
-      <form action="/message/crews-logout" method="post">
-        @csrf
-        <input type="submit" class="btnType3" value="ログアウト">
-      </form>
+      <div>
+          <form action="/message/crews-logout" id="logoutForm" method="post">
+          @csrf
+          <input type="button" class="btnType3 crewLogout" id="crewLogout" value="ログアウト">
+          </form>
+        <div>{{$check_crew[0]->part_code ?? ""}} {{$check_crew[0]->name ?? ""}}さんの未読/既読を表示中です。</div>
+      </div>
     @else
       <button type="button" class="btnType3 btnModal" data-modal-target="check">ユーザー選択</button>
     @endif
