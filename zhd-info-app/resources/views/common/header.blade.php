3a2d9a480543e837c2d8112566af40558004718f
mshodai <mshodai@nssx.co.jp>
2024-02-21 13:45:22 +0900

    ヘッダーの高さ調整

--- zhd-info-app/resources/views/common/header.blade.php
+++ zhd-info-app/resources/views/common/header.blade.php
@@ -5,9 +5,11 @@
         <div>
             <form action="/message/crews-logout" id="logoutForm" method="post">
             @csrf
-            <button type="button" class="btnType3 crewLogout" id="crewLogout">ログアウト</button>
+            <div>
+            <button style="padding: 0 10px 0 10px type="button" class="btnType3 crewLogout" id="crewLogout">ログアウト</button>
+            <div style="padding-top: 10px ">{{$check_crew[0]->part_code ?? ""}} {{$check_crew[0]->name ?? ""}}さんの未読/既読を表示中です。</div>
+            </div>
             </form>
-          <div>{{$check_crew[0]->part_code ?? ""}} {{$check_crew[0]->name ?? ""}}さんの未読/既読を表示中です。</div>
         </div>
       @else
         <button type="button" class="btnType3 btnModal" data-modal-target="check">自分の閲覧状況の確認</button>
