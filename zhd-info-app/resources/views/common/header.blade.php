0d479cd8fdc4f4e04163ec89617acbc9ef56c3e3
mshodai <mshodai@nssx.co.jp>
2024-02-21 12:39:18 +0900

    ヘッダーの一部出しわけ

--- zhd-info-app/resources/views/common/header.blade.php
+++ zhd-info-app/resources/views/common/header.blade.php
@@ -1,15 +1,17 @@
 <header class="header">
   <div class="header__inner">
-    @if (session('check_crew'))
-      <div>
-          <form action="/message/crews-logout" id="logoutForm" method="post">
-          @csrf
-          <button type="button" class="btnType3 crewLogout" id="crewLogout">ログアウト</button>
-          </form>
-        <div>{{$check_crew[0]->part_code ?? ""}} {{$check_crew[0]->name ?? ""}}さんの未読/既読を表示中です。</div>
-      </div>
-    @else
-      <button type="button" class="btnType3 btnModal" data-modal-target="check">自分の閲覧状況の確認</button>
+    @if (request()->is('/') || request()->is('message'))
+      @if (session('check_crew'))
+        <div>
+            <form action="/message/crews-logout" id="logoutForm" method="post">
+            @csrf
+            <button type="button" class="btnType3 crewLogout" id="crewLogout">ログアウト</button>
+            </form>
+          <div>{{$check_crew[0]->part_code ?? ""}} {{$check_crew[0]->name ?? ""}}さんの未読/既読を表示中です。</div>
+        </div>
+      @else
+        <button type="button" class="btnType3 btnModal" data-modal-target="check">自分の閲覧状況の確認</button>
+      @endif
     @endif
-    </div>
+  </div>
 </header>
