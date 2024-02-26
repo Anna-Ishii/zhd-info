a5d5fa8b4a345137b3e58e159cc0bd55a3dfb27d
mshodai <mshodai@nssx.co.jp>
2024-02-26 19:39:28 +0900

    カラムの結合をしないよう変更

--- zhd-info-app/public/js/admin/analyse/personal.js
+++ zhd-info-app/public/js/admin/analyse/personal.js
@@ -1,32 +1,3 @@
-
-function mergeRowCell(targetCells) {
-    var counter = 0;
-	var text ="";
-	var target="";
-
-	$(targetCells).each(function(index) {
-	  if ($(this).text() == text) {
-		counter++;
-		if(target !="")
-			target.remove();
-	  } else  {
-		if(target !="")
-			target.attr('rowSpan', counter);
-		counter=1;
-	  }
-	  text = $(this).text();
-	  target = $(this);
-      if (targetCells.length - 1 == index) target.attr('rowSpan', counter);
-	});
-
-}
-$(function() {
-    mergeRowCell($('td.orgDS','tr').get().reverse());
-	mergeRowCell($('td.orgAR','tr').get().reverse());
-    mergeRowCell($('td.orgBL','tr').get().reverse());
-});
-
-
 $(window).on('load' , function(){
 	let d = new Date();
 	/* datetimepicker */
