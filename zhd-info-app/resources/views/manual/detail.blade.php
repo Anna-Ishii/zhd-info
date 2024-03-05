04321ea8b5c83bc33e63ddd39a5bcff279bb3302
mshodai <mshodai@nssx.co.jp>
2024-03-05 10:51:21 +0900

    未使用の機能の廃止、iconの削除

--- zhd-info-app/resources/views/manual/detail.blade.php
+++ zhd-info-app/resources/views/manual/detail.blade.php
@@ -17,11 +17,6 @@
             </div>
             <ul class="header__menu flex">
                 <li>
-                    <a href="#" class="btnMoveFolder" data-target-name="moveFolder">
-                        <img src="{{ asset('img/icon_folder_open.svg') }}" alt="">
-                    </a>
-                </li>
-                <li>
                     <button type="button" class="btnPrint">
                         <img src="{{ asset('img/icon_print.svg') }}" alt="印刷する">
                     </button>
