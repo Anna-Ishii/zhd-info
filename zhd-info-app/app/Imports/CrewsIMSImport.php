fe6ddfb3884e6dce06efad20bdf71aac331f5d04
nssxhonda <122953174+nssxhonda@users.noreply.github.com>
2024-07-08 14:37:39 +0900

    業態追加対応

--- zhd-info-app/app/Imports/CrewsIMSImport.php
+++ zhd-info-app/app/Imports/CrewsIMSImport.php
@@ -7,17 +7,31 @@ use Maatwebsite\Excel\Concerns\Importable;
 use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\WithCalculatedFormulas;
 use Maatwebsite\Excel\Concerns\WithStartRow;
+use Maatwebsite\Excel\Concerns\WithChunkReading;
+
+use App\Console\Commands\ImportImsCsvCommand;
+
 
 class CrewsIMSImport implements
     ToCollection,
     WithCalculatedFormulas,
-    WithStartRow
+    WithStartRow,
+    WithChunkReading
 {
     use Importable;
 
+    protected $command;
+
+    public function __construct(ImportImsCsvCommand $command)
+    {
+        $this->command = $command;
+    }
+
     public function collection(Collection $rows)
     {
+        $this->command->import_crews($rows);
     }
+
     public function headingRow(): int
     {
         return 1;
@@ -31,5 +45,8 @@ class CrewsIMSImport implements
         return 2;
     }
 
-
+    public function chunkSize(): int
+    {
+        return 10000;
+    }
 }
