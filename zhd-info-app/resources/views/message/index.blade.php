c784a76a21b64e2f2ae25e61c4aca5de1ad759bd
nssxhonda <122953174+nssxhonda@users.noreply.github.com>
2024-08-29 22:55:22 +0900

    業態追加対応|Huawei端末動作確認|Chromeで業連ファイル(pdf)を表示出来るようにする

--- zhd-info-app/resources/views/message/index.blade.php
+++ zhd-info-app/resources/views/message/index.blade.php
@@ -155,7 +155,6 @@
                     <div class="modal-body" style="padding: 10px;">
                         @foreach ($message->content_files as $file)
                             <button type="button" class="btnType3" style="margin-left: unset; display: block; margin-bottom: 10px;">
-                                {{-- <a href="{{ asset($file['file_url']) }}" target="_blank" style="display: block; line-height: 35px;">{{ $file['file_name'] }}</a> --}}
                                 <a href="{{ route('message.detail', ['message_id' => $message->id, 'message_content_url' => $file['file_url']]) }}" style="display: block; line-height: 35px;">{{ $file['file_name'] }}</a>
                             </button>
                         @endforeach
