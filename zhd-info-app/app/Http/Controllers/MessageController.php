66b31e7987066b1f78981582102194911e30191e
nssxhonda <122953174+nssxhonda@users.noreply.github.com>
2024-09-25 18:57:56 +0900

    一覧画面を表示する速度の改善

--- zhd-info-app/app/Http/Controllers/MessageController.php
+++ zhd-info-app/app/Http/Controllers/MessageController.php
@@ -288,7 +288,6 @@ class MessageController extends Controller
     public function getCrewsMessage(Request $request)
     {
         ini_set('memory_limit', '1024M'); // メモリ制限を一時的に増加
-        ini_set('max_execution_time', 300); // 実行時間を一時的に300秒に設定
 
         $message = $request->input('message');
         $text = $request->input('text');
@@ -347,9 +346,8 @@ class MessageController extends Controller
             ->orderBy('c.name_kana', 'asc')
             ->get();
 
-        // メモリ制限と実行時間をデフォルトの設定に戻す
+        // デフォルトの設定に戻す
         ini_restore('memory_limit');
-        ini_restore('max_execution_time');
 
         return response()->json([
             'crews' => $crews,
