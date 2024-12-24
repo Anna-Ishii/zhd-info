779dc418822172e85a9a77d1e0fe31455fa6329d
nssxhonda <122953174+nssxhonda@users.noreply.github.com>
2024-12-24 14:10:05 +0900

    DM/BMへ閲覧状況のサマリをメール送信

--- zhd-info-app/app/Utils/SESMailer.php
+++ zhd-info-app/app/Utils/SESMailer.php
@@ -3,7 +3,7 @@
 namespace App\Utils;
 
 use Aws\Ses\SesClient;
-use Aws\Exception\AwsException;
+use Aws\Ses\Exception\SesException;
 
 class SESMailer
 {
@@ -17,37 +17,52 @@ class SESMailer
         ]);
     }
 
-    public function sendEmail($to, $subject, $message)
+    public function sendEmail($to, $subject, $message, $filePath = null)
     {
         try {
-            // 送信者名とメールアドレスを設定
             $fromAddress = 'zhd-gyoren-system@zensho.com';
             $fromName = 'システム管理者';
             $from = '=?UTF-8?B?' . base64_encode($fromName) . '?= <' . $fromAddress . '>';
 
-            $this->sesClient->sendEmail([
-                'Destination' => [
-                    'ToAddresses' => $to,
-                ],
-                'Message' => [
-                    'Body' => [
-                        'Text' => [
-                            'Charset' => 'UTF-8',
-                            'Data' => $message,
-                        ],
-                    ],
-                    'Subject' => [
-                        'Charset' => 'UTF-8',
-                        'Data' => $subject,
-                    ],
+            $boundary = uniqid('np');
+
+            $headers = "MIME-Version: 1.0\r\n";
+            $headers .= "From: $from\r\n";
+            $headers .= "To: " . implode(',', $to) . "\r\n";
+            $headers .= "Subject: $subject\r\n";
+            $headers .= "Content-Type: multipart/mixed; boundary=\"" . $boundary . "\"\r\n";
+
+            $body = "--" . $boundary . "\r\n";
+            $body .= "Content-Type: text/plain; charset=UTF-8\r\n";
+            $body .= "Content-Transfer-Encoding: 7bit\r\n\r\n";
+            $body .= $message . "\r\n";
+
+            if ($filePath) {
+                $attachment = new \SplFileInfo($filePath);
+                $attachmentPath = $attachment->getRealPath();
+                $attachmentContent = file_get_contents($attachmentPath);
+                $attachmentBase64 = base64_encode($attachmentContent);
+
+                $body .= "--" . $boundary . "\r\n";
+                $body .= "Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet; name=\"" . $attachment->getFilename() . "\"\r\n";
+                $body .= "Content-Transfer-Encoding: base64\r\n";
+                $body .= "Content-Disposition: attachment; filename=\"" . $attachment->getFilename() . "\"\r\n\r\n";
+                $body .= $attachmentBase64 . "\r\n";
+            }
+
+            $body .= "--" . $boundary . "--";
+
+            $result = $this->sesClient->sendRawEmail([
+                'RawMessage' => [
+                    'Data' => $headers . "\r\n" . $body,
                 ],
                 'Source' => $from,
                 'SourceArn' => 'arn:aws:ses:us-west-2:016712135282:identity/zhd-gyoren-system@zensho.com',
             ]);
 
             return true;
-        } catch (AwsException $e) {
-            error_log($e->getMessage()); // エラーメッセージをログに出力
+        } catch (SesException $e) {
+            error_log($e->getMessage());
             return false;
         }
     }
