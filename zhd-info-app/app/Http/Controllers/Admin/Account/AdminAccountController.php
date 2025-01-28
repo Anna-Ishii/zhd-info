c1943f4625ccb417915d4986343bef62d1b222e1
nssxhonda <122953174+nssxhonda@users.noreply.github.com>
2025-01-28 19:52:27 +0900

    ユーザーごとに各画面の検索条件のデフォルト値を設定できるようにする

--- zhd-info-app/app/Http/Controllers/Admin/Account/AdminAccountController.php
+++ zhd-info-app/app/Http/Controllers/Admin/Account/AdminAccountController.php
@@ -52,12 +52,35 @@ class AdminAccountController extends Controller
 
     public function new()
     {
+        $admin = session('admin');
+
         $organization1_list = Organization1::orderby('name')->get();
         $adminpage_list = Adminpage::orderby('name')->get();
+
+        // 検索条件を取得
+        $message_saved_url = SearchCondition::where('admin_id', $admin->id)
+            ->where('page_name', 'message-publish')
+            ->where('deleted_at', null)
+            ->select('page_name', 'url')
+            ->first();
+        $manual_saved_url = SearchCondition::where('admin_id', $admin->id)
+            ->where('page_name', 'manual-publish')
+            ->where('deleted_at', null)
+            ->select('page_name', 'url')
+            ->first();
+        $analyse_personal_saved_url = SearchCondition::where('admin_id', $admin->id)
+            ->where('page_name', 'analyse-personal')
+            ->where('deleted_at', null)
+            ->select('page_name', 'url')
+            ->first();
+
         return view('admin.account.admin.new', [
             'organization1_list' => $organization1_list,
             'ability_list' => AdminAbility::cases(),
-            'adminpage_list' => $adminpage_list
+            'adminpage_list' => $adminpage_list,
+            'message_saved_url' => $message_saved_url,
+            'manual_saved_url' => $manual_saved_url,
+            'analyse_personal_saved_url' => $analyse_personal_saved_url,
         ]);
     }
 
@@ -91,16 +114,39 @@ class AdminAccountController extends Controller
 
     public function edit($admin_id)
     {
+        $adminId = session('admin');
+
         $admin = Admin::withTrashed()
             ->where('id', $admin_id)
             ->first();
         $organization1_list = Organization1::orderby('name')->get();
         $adminpage_list = Adminpage::orderby('name')->get();
+
+        // 検索条件を取得
+        $message_saved_url = SearchCondition::where('admin_id', $adminId->id)
+            ->where('page_name', 'message-publish')
+            ->where('deleted_at', null)
+            ->select('page_name', 'url')
+            ->first();
+        $manual_saved_url = SearchCondition::where('admin_id', $adminId->id)
+            ->where('page_name', 'manual-publish')
+            ->where('deleted_at', null)
+            ->select('page_name', 'url')
+            ->first();
+        $analyse_personal_saved_url = SearchCondition::where('admin_id', $adminId->id)
+            ->where('page_name', 'analyse-personal')
+            ->where('deleted_at', null)
+            ->select('page_name', 'url')
+            ->first();
+
         return view('admin.account.admin.edit', [
             'organization1_list' => $organization1_list,
             'ability_list' => AdminAbility::cases(),
             'adminpage_list' => $adminpage_list,
-            'edit_admin' => $admin
+            'edit_admin' => $admin,
+            'message_saved_url' => $message_saved_url,
+            'manual_saved_url' => $manual_saved_url,
+            'analyse_personal_saved_url' => $analyse_personal_saved_url,
         ]);
     }
 
