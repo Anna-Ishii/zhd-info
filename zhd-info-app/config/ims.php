40ae1f372904ec5bcacc0191d57623342340e50a
大竹裕介 <yotake@nssx.co.jp>
2025-07-28 13:41:01 +0900

    ims.php修正

--- zhd-info-app/config/ims.php
+++ zhd-info-app/config/ims.php
@@ -2,10 +2,5 @@
 return [
     'test_date' => env('IMS_TEST_DATE', null),
     's3_directory' => env('IMS_S3_DIRECTORY', 'IMS2/FR_BUSINESS/'),
-];
-
-return [
-    ・・・
     'job_timeout' => env('IMS_JOB_TIMEOUT', 5400),
-    ・・・
 ];
