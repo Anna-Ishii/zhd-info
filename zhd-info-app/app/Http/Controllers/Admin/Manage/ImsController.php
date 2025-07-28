4fe310e8c9f395a9b24f624243895750e56386c9
大竹裕介 <yotake@nssx.co.jp>
2025-07-28 14:36:48 +0900

    ImsController.php修正

--- zhd-info-app/app/Http/Controllers/Admin/Manage/ImsController.php
+++ zhd-info-app/app/Http/Controllers/Admin/Manage/ImsController.php
@@ -49,44 +49,70 @@ class ImsController extends Controller
     	
     // 	//Storage::disk('local')->put('sample.txt', "test");
     	
-    //     $admin = session('admin');
-    //     $log = ImsSyncLog::orderBy('id', 'desc')->limit(30)->get();
+        // $admin = session('admin');
+        // $log = ImsSyncLog::orderBy('id', 'desc')->limit(30)->get();
 
-    //     // 検索条件を取得
-    //     $message_saved_url = SearchCondition::where('admin_id', $admin->id)
-    //         ->where('page_name', 'message-publish')
-    //         ->where('deleted_at', null)
-    //         ->select('page_name', 'url')
-    //         ->first();
-    //     $manual_saved_url = SearchCondition::where('admin_id', $admin->id)
-    //         ->where('page_name', 'manual-publish')
-    //         ->where('deleted_at', null)
-    //         ->select('page_name', 'url')
-    //         ->first();
-    //     $analyse_personal_saved_url = SearchCondition::where('admin_id', $admin->id)
-    //         ->where('page_name', 'analyse-personal')
-    //         ->where('deleted_at', null)
-    //         ->select('page_name', 'url')
-    //         ->first();
+        // // 検索条件を取得
+        // $message_saved_url = SearchCondition::where('admin_id', $admin->id)
+        //     ->where('page_name', 'message-publish')
+        //     ->where('deleted_at', null)
+        //     ->select('page_name', 'url')
+        //     ->first();
+        // $manual_saved_url = SearchCondition::where('admin_id', $admin->id)
+        //     ->where('page_name', 'manual-publish')
+        //     ->where('deleted_at', null)
+        //     ->select('page_name', 'url')
+        //     ->first();
+        // $analyse_personal_saved_url = SearchCondition::where('admin_id', $admin->id)
+        //     ->where('page_name', 'analyse-personal')
+        //     ->where('deleted_at', null)
+        //     ->select('page_name', 'url')
+        //     ->first();
 
 
-    //     return view('admin.manage.ims', [
-    //         'log' => $log,
-    //         'message_saved_url' => $message_saved_url,
-    //         'manual_saved_url' => $manual_saved_url,
-    //         'analyse_personal_saved_url' => $analyse_personal_saved_url,
-    //     ]);
+        // return view('admin.manage.ims', [
+        //     'log' => $log,
+        //     'message_saved_url' => $message_saved_url,
+        //     'manual_saved_url' => $manual_saved_url,
+        //     'analyse_personal_saved_url' => $analyse_personal_saved_url,
+        // ]);
     // }
 
     // コントローラー（ImsController.php）
     public function index()
     {
+        $admin = session('admin');
+        $log = ImsSyncLog::orderBy('id', 'desc')->limit(30)->get();
+
+        // 検索条件を取得
+        $message_saved_url = SearchCondition::where('admin_id', $admin->id)
+            ->where('page_name', 'message-publish')
+            ->where('deleted_at', null)
+            ->select('page_name', 'url')
+            ->first();
+        $manual_saved_url = SearchCondition::where('admin_id', $admin->id)
+            ->where('page_name', 'manual-publish')
+            ->where('deleted_at', null)
+            ->select('page_name', 'url')
+            ->first();
+        $analyse_personal_saved_url = SearchCondition::where('admin_id', $admin->id)
+            ->where('page_name', 'analyse-personal')
+            ->where('deleted_at', null)
+            ->select('page_name', 'url')
+            ->first();
+
         $isJobRunning = DB::table('jobs')
             ->where('payload', 'like', '%importjob%')
             ->exists();
             
         $log = ImsSyncLog::orderBy('import_at', 'desc')->get();
         
-        return view('admin.manage.ims', compact('log', 'isJobRunning'));
+        return view('admin.manage.ims', [
+            'log' => $log,
+            'message_saved_url' => $message_saved_url,
+            'manual_saved_url' => $manual_saved_url,
+            'analyse_personal_saved_url' => $analyse_personal_saved_url,
+            'isJobRunning' => $isJobRunning,
+        ]);
     }
 }
