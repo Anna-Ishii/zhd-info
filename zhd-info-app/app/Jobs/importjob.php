66fe503995fa814b97ea10a96e0fc44a3d37904a
大竹裕介 <yotake@nssx.co.jp>
2025-08-07 10:14:05 +0900

    importjobタイムアウト設定変更

--- zhd-info-app/app/Jobs/importjob.php
+++ zhd-info-app/app/Jobs/importjob.php
@@ -9,25 +9,29 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Artisan;
+
 class importjob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
-    public $timeout;
+    public $timeout= 5400;
+    public $tries = 1;
 
     /**
      * Create a new job instance.
      */
-    public function __construct()
-    {
-        $this->timeout = config('ims.job_timeout', 5400);
-    }
+    // public function __construct()
+    // {
+    //     $this->timeout = config('ims.job_timeout', 7200);
+    // }
 
     /**
      * Execute the job.
      */
     public function handle(): void
     {
-        //
+        ini_set('max_execution_time', '7200');
+        set_time_limit(7200);
         Artisan::call('app:import-ims-csv-command');
     }
 }
+
