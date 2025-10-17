6b2bf4fb4d6ebab4c49be65eb77fd72b3a483259
Anna-Ishii <aishii@nssx.co.jp>
2025-10-17 12:20:23 +0900

    WIP: local changes before switching to phase3

--- zhd-info-app/app/Providers/AppServiceProvider.php
+++ zhd-info-app/app/Providers/AppServiceProvider.php
@@ -11,11 +11,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        if ($this->app->environment('local')) {
-            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
-            $this->app->register(TelescopeServiceProvider::class);
-        }
-        \Laravel\Telescope\Telescope::ignoreMigrations();
+        // if ($this->app->environment('local')) {
+        //     $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
+        //     $this->app->register(TelescopeServiceProvider::class);
+        // }
+        // \Laravel\Telescope\Telescope::ignoreMigrations();
     }
 
     /**
