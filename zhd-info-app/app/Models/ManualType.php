e56b0f12cfe2daf6442ac015e89c6e396d684b8f
skomine-nssx <skomine@nssx.co.jp>
2025-10-17 17:58:34 +0900

    形式項目追加

---
+++ zhd-info-app/app/Models/ManualType.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ManualType extends Model
+{
+    use HasFactory;
+
+    protected $tables = 'manual_types';
+
+    protected $fillable =
+    [
+        'name',
+    ];
+}
