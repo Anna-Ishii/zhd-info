7566750d9af9bc97428753db3bc9068ca723edb0
AzureAD+野﨑凌 <rnozaki@nssx.co.jp>
2025-10-23 04:48:58 +0900

    編集ページの親の削除ボタンの名称をdeletePrepareBtnに変更

--- zhd-info-app/resources/views/admin/message/publish/edit.blade.php
+++ zhd-info-app/resources/views/admin/message/publish/edit.blade.php
@@ -512,7 +512,7 @@
 
     <div class="footer" style="margin-left: 0;">
         <p><a href="/admin/message/publish?{{ session('message_publish_url') }}">一覧に戻る</a></p>
-        <button class="c-btn__grey" type="button" id="deleteBtn" onclick="confirmDelete()">削除</button>
+        <button class="c-btn__grey" type="button" id="deletePrepareBtn" onclick="confirmDelete()">削除</button>
         @if ($message->editing_flg)
             <button class="c-btn__white" type="submit" name="save" form="form" onclick="window.onbeforeunload=null">保存</button>
         @endif
